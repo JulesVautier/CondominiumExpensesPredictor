516d5b5e79f5a7d0dc626dee562d1236328e3bc7
jules vautier <jules.vautier@epitech.eu>
2018-11-14 17:05:03 +0100

    init architecture

---
+++ __main__.py
@@ -0,0 +1,10 @@
+from app import DataHelper
+
+
+def run_project():
+    dataHelper = DataHelper()
+    dataHelper.start()
+
+
+if __name__ == '__main__':
+    run_project()
---
+++ app/DataHelper.py
@@ -0,0 +1,10 @@
+DATA_PATH = '../ressources/dataset_annonces.csv'
+
+class DataHelper:
+
+    def __init__(self, data_path=DATA_PATH):
+        self.data_path = data_path
+        pass
+
+    def start(self):
+        pass
