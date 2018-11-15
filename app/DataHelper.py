1411d3eea453a1ccac08dc8842eaff282a8c927c
jules vautier <jules.vautier@epitech.eu>
2018-11-15 23:20:15 +0100

    decorator + delete

--- app/DataHelper.py
+++ app/DataHelper.py
@@ -3,6 +3,20 @@ import pandas as pd
 DATA_PATH = './ressources/dataset_annonces.csv'
 
 
+def check(func):
+    def wrapper(self, id):
+        try:
+            print('try ', id)
+            self.df.loc[id]
+        except:
+            print('except ', id)
+            return 401
+        print('ok ', id)
+        return func(self, id)
+
+    return wrapper
+
+
 class DataHelper:
 
     def __init__(self, data_path=DATA_PATH):
@@ -10,18 +24,34 @@ class DataHelper:
         self.data_path = data_path
         self.pd = pd
         self.readData()
+        self.cleanData()
         pass
 
     def readData(self):
+        print('read_data')
         self.df = self.pd.read_csv(self.data_path, nrows=100)
+
+    def cleanData(self):
+        # self.df.drop(['ID'], inplace=True, axis=1)
+        self.df['ID'] = self.df.index
+
+    def toJson(self, data):
+        return data.to_json()
 
     def getDataFrame(self):
         return self.df
 
     def getDatas(self, limite_one, limite_two):
-        return self.df.loc[limite_one:limite_two]
+        return self.toJson(self.df.loc[limite_one:limite_two])
 
+    @check
     def getData(self, id):
-        return self.df.loc[[id]]
+        return self.toJson(self.df.loc[id])
+
+    @check
+    def deleteData(self, id):
+        self.df.drop(id, inplace=True)
+        return 200
+
 
 dataHelper = DataHelper()
--- app/api.py
+++ app/api.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 import subprocess
 import os
 
+from flask.json import jsonify
+
 from app import dataHelper
 
 app = Flask(__name__)
@@ -14,13 +16,22 @@ def HelloWorld():
 
 @app.route("/datas", methods=['GET'])
 def getDatas():
-    limite_one = request.args.get('limite_one', default=None, type=int)
-    limite_two = request.args.get('limite_two', default=None, type=int)
-    resp = dataHelper.getDatas(limite_one, limite_two)
-    return str(resp)
+    limit_one = request.args.get('limite_one', default=None, type=int)
+    limit_two = request.args.get('limite_two', default=None, type=int)
+    resp = dataHelper.getDatas(limit_one, limit_two)
+    return jsonify(resp)
 
 
 @app.route("/data/<int:id>", methods=['GET'])
 def getData(id):
     resp = dataHelper.getData(id)
-    return str(resp)
+    return jsonify(resp)
+
+@app.route("/data/<int:id>", methods=['DELETE'])
+def deleteData(id):
+    resp = dataHelper.deleteData(id)
+    return jsonify(resp)
+
+@app.route("/data", methods=['POST'])
+def post_one_row():
+    
